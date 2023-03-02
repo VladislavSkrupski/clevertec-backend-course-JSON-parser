e1bfdf15bbf25dd6e5412dcd0f324bef53735f54
VladislavSkrupski <vladislav.skrupski@gmail.com>
2023-03-02 23:32:50 +0300

    feat: add tests for fromJSON method

--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -3,17 +3,16 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.exceptions.base.MockitoException;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class JSONParserTest {
@@ -28,7 +27,7 @@ class JSONParserTest {
     @Nested
     class toJSONTest {
         @ParameterizedTest
-        @MethodSource("dataProvider")
+        @MethodSource("JSONParserTest#dataProvider")
         void toJSONShouldReturnExpectedString(Object object, String expected) {
             String actual = JSONParser.toJSON(object);
 
@@ -59,30 +58,66 @@ class JSONParserTest {
 
             assertThat(actual).isEqualTo(expected);
         }
-
-        private static Stream<Arguments> dataProvider() {
-            Gson gson = new Gson();
-            TestProbe testProbe = new TestProbe();
-            TestProbeHelper testProbeHelper = new TestProbeHelper();
-
-            String expected1 = gson.toJson(testProbe);
-            String expected2 = gson.toJson(testProbeHelper);
-            testProbe.clear();
-            String expected3 = gson.toJson(testProbe);
-
-            return Stream.of(
-                    Arguments.of(new TestProbe(), expected1),
-                    Arguments.of(new TestProbeHelper(), expected2),
-                    Arguments.of(testProbe, expected3)
-            );
-        }
     }
 
     @Nested
     class fromJSONTest {
-        @Test
-        void fromJSON() {
+        @ParameterizedTest
+        @MethodSource("JSONParserTest#dataProvider")
+        void fromJSONShouldReturnExpectedObject(Object object, String json) {
+            Object actual = JSONParser.fromJSON(json, object);
+            Object expected = gson.fromJson(json, object.getClass());
 
+            assertThat(gson.toJson(actual)).isEqualTo(gson.toJson(expected));
         }
+
+        @ParameterizedTest
+        @NullSource()
+        void fromJSONShouldReturnNull(Object object) {
+            Object actual = JSONParser.fromJSON("", object);
+
+            assertThat(actual).isNull();
+        }
+
+        @Test
+        void fromJSONShouldReturnExpectedStringForTestProbeMock() {
+            String jsonA = JSONParser.toJSON(testProbeMock);
+            String jsonE = gson.toJson(testProbeMock);
+
+            Object actual = JSONParser.fromJSON(jsonA, testProbeMock);
+            Mockito.reset(testProbeMock);
+            Object expected = gson.fromJson(jsonE, testProbeMock.getClass());
+
+            assertThat(gson.toJson(actual)).isEqualTo(gson.toJson(expected));
+        }
+
+        @Test
+        void fromJSONShouldReturnExpectedStringForTestProbeHelperMock() {
+            String jsonA = JSONParser.toJSON(testProbeHelperMock);
+            String jsonE = gson.toJson(testProbeHelperMock);
+
+            Object actual = JSONParser.fromJSON(jsonA, testProbeHelperMock);
+            Mockito.reset(testProbeHelperMock);
+            Object expected = gson.fromJson(jsonE, testProbeHelperMock.getClass());
+
+            assertThat(gson.toJson(actual)).isEqualTo(gson.toJson(expected));
+        }
+    }
+
+    private static Stream<Arguments> dataProvider() {
+        Gson gson = new Gson();
+        TestProbe testProbe = new TestProbe();
+        TestProbeHelper testProbeHelper = new TestProbeHelper();
+
+        String json1 = gson.toJson(testProbe);
+        String json2 = gson.toJson(testProbeHelper);
+        testProbe.clear();
+        String json3 = gson.toJson(testProbe);
+
+        return Stream.of(
+                Arguments.of(new TestProbe(), json1),
+                Arguments.of(new TestProbeHelper(), json2),
+                Arguments.of(testProbe, json3)
+        );
     }
 }
